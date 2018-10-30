d4d2bb6af2445db2c93cfcdf10c727d6c96c9095
StarZoOm <kulevkir@gmail.com>
2018-10-30 11:16:20 +0300

    moving ship, shooting

--- bullet.py
+++ bullet.py
@@ -3,7 +3,7 @@ from pygame.sprite import Sprite
 
 class Bullet(Sprite):
     def __init__(self, ai_settigs, screen, ship):
-        super(Bullet, self).__init__()
+        super().__init__()
         self.screen = screen
 
         self.rect = pygame.Rect(0, 0, ai_settigs.bullet_width, ai_settigs.bullet_height)
@@ -14,3 +14,10 @@ class Bullet(Sprite):
 
         self.color = ai_settigs.bullet_color
         self.speed_factor = ai_settigs.bullet_speed_factor
+
+    def update(self):
+        self.y -= self.speed_factor
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
