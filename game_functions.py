103c9146b30c38bcd4115a9997c2ff88f09ce421
StarZoOm <kulevkir@gmail.com>
2018-10-30 13:21:45 +0300

    moving ship, shooting

--- game_functions.py
+++ game_functions.py
@@ -19,18 +19,15 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # При нажатии пробела, создается новый экземпляр класса пули и добавляется в список пуль
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
-
+        fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
-
 
 def update_screen(ai_settings, screen, ship, bullets):
     screen.fill(ai_settings.bg_color)
@@ -45,3 +42,9 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    # При нажатии пробела, создается новый экземпляр класса пули и добавляется в список пуль
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
